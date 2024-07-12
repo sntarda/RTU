2dd08cdd4c45eef98e7b2baa49225d6771bd6780
Samir Tarda <115050984+sntarda@users.noreply.github.com>
2024-07-12 08:06:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pickle
 from pathlib import Path
 
 import pandas as pd
-import plotly.express as px  # Corrected this line
+import plotly.express as px
 import streamlit as st
 import streamlit_authenticator as stauth
 
